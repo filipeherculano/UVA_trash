cc05d5a7dc1ce927e225d12c097d0b7bde7d0343
Filipe <filipeherculanorocha@gmail.com>
2016-09-12 00:14:10 -0300

    Patterns and THE F*CKING 10114

--- 10114.cpp
+++ 10114.cpp
@@ -7,21 +7,21 @@ int main(){
 	double loan, down;
 	while(cin >> mon >> down >> loan >> dep && mon > 0){
 		int index;
-		double temp, actualLoan = loan, carValue = loan, p;
+		double temp, actualLoan = loan, carValue = loan+down, p, payoff = loan / (double)mon;
 		map<int, double> depMon;
 		map<int, double>::iterator it;
 		for(int i = 0; i < dep; i++){
 			cin >> index >> temp;
 			depMon.insert(std::pair<int, double>(index, (1.0 - temp)));
 		}
-		for(index = 0; index < mon && actualLoan >= carValue; index++){
+		for(index = 0; index <= mon ; index++){
 			it = depMon.find(index);
 			if(it != depMon.end()) p = it->second;
-			actualLoan -= down;
+			if(index) actualLoan -= payoff;
 			carValue *= p;
-			printf("%lf %lf %lf\n", actualLoan, carValue, p);
+			if(actualLoan < carValue) break;
 		}
-		printf("%d %s\n", index, index > 1 ? "months" : "month");
+		printf("%d %s\n", index, index == 1 ? "month" : "months");
 	}
 	return 0;
 }
---
+++ Pattern.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+#typedef unsigned long long llu
+#typedef long long ll
+#typedef long double Lf
+
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
