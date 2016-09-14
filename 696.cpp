669d5d419652b7b2db6ced1d5f15f507644a0497
Filipe <filipeherculanorocha@gmail.com>
2016-09-14 01:34:25 -0300

    adding some comments

--- 696.cpp
+++ 696.cpp
@@ -6,6 +6,28 @@
 
 using namespace std;
 
+/*
+	This question need some clarifications:
+		-> When one of the sides of the chessboard is equal to one,
+		   then the maximum number of knights is equal to the other
+		   side. Since knights can't move in straight lines.
+		-> When one of the sides of the chessboard is equal to two,
+		   we will calculate differently. I noticed that for a che-
+		   ssboard with one of the sides equal to 2, the rule below
+		   doesn't apply. The rule now is: "Every 4 slots in one row
+		   we'll have 2 knights". This means that a side that has
+		   mod != 1 we should just add three(adding ceil) and divide 
+		   by 4. If it is equal to 1, then subtract by one.
+		-> The trivial answer is just counting the number of slots
+		   in the same color, since knights can only move to other
+		   color slots. For example: in a 5x5 chessboard, there is
+		   exactly 25 slots, 12 black and 13 white (or the other
+		   way around). Which means the maximum number of knights
+		   in this trivial test case is 13.
+
+	To really understand this question, you should draw alot! Good luck!
+*/
+
 int main(){
 	int m, n;
 	while(cin >> m >> n && (m || n)){
